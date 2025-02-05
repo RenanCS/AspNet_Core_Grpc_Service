03c75c19a4655d256d9ca8884494ba35027f2821
Renan Carvalho de Souza <renancarvalhodesouza@gmail.com>
2025-02-05 18:07:47 -0300

    feat: create console application client

---
+++ ConsoleApp_Grpc_Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using AspNet_Core_Grpc_Service;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5279");
+
+var client1 = new Notifier.NotifierClient(channel);
+var request1 = new SubscriptionRequest { Topic = "Customer One" };
+_ = Task.Run(() => ReadNotifications(client1, request1));
+
+var client2 = new Notifier.NotifierClient(channel);
+var request2 = new SubscriptionRequest { Topic = "Customer Two" };
+_ = Task.Run(() => ReadNotifications(client2, request2));
+
+Console.WriteLine("Registered customers. Waiting for notifications...");
+Console.ReadKey();
+
+static async Task ReadNotifications(Notifier.NotifierClient client, SubscriptionRequest request)
+{
+    try
+    {
+        using var call = client.Subscribe(request);
+        await foreach (var notification in call.ResponseStream.ReadAllAsync())
+        {
+            Console.WriteLine($"Customer {client.GetHashCode()} - New notification for => {notification.Topic}: {notification.Message}");
+        }
+    }
+    catch (Grpc.Core.RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.Cancelled)
+    {
+        Console.WriteLine($"Customer {client.GetHashCode()} - Registration canceled by the server");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Customer {client.GetHashCode()} - Error receiving notifications: {ex.Message}");
+    }
+    finally
+    {
+        Console.WriteLine($"Customer {client.GetHashCode()} - End of registration.");
+    }
+}
